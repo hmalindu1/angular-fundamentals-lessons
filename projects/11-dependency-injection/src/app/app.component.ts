04fe885af6eca13aec16a9d288c19ae5b1f8ee4c
Hashan <hemachandra.hashan@gmail.com>
2024-03-21 05:47:49 +0530

    component corrected

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { UserInfoComponent } from './user-info/user-info.component';
   template: ` 
   <h1>User Listing</h1>
   @for (user of userData; track user.id) {
-    <app-user-info [user]="user"></app-user-info>
+    <app-user-info [user]="user"/>
   }
   `,
 })
